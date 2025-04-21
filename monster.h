b419af5256d86405336cc5c0b6cbf725496b267b
dacotafab <dacotafab@gmail.com>
2025-04-21 10:27:18 -0500

    Filled out player & monster a bit

--- monster.h
+++ monster.h
@@ -1,3 +1,13 @@
+#pragma once
 #include <string>
+#include "character.h"
 
 using namespace std;
+
+class Monster : public Character {
+    private:
+
+    public:
+        Monster();
+        void randomAbility();
+};
--- player.h
+++ player.h
@@ -1,4 +1,14 @@
+#pragma once
 #include <string>
+#include "character.h"
 
 using namespace std;
+
+class Player : public Character {
+    private:
+
+    public:
+        Player();
+        void userInput();
+};
 
